8c14e46a9ef57e3e95401030fc489c52b101c239
Dainis Tillers <dainis.tillers@gmail.com>
2012-03-05 22:00:05 +0200

    added: more css goodies

--- js.js
+++ js.js
@@ -1,3 +1,16 @@
 $(document).ready(function(){
+	$('#settings_modal').modal({backdrop: 'static', keyboard: false, show: true});
 
+	$('#settings_ok_button').on('click', function(){
+		$('#settings_modal').modal('hide');
+		return false;
+	});
+
+	var new_process_modal = $('#new_process_modal').modal({backdrop: 'static', keyboard: false}).modal('hide');
+
+	$('#new_process').on('click', function(){
+		console.log(123);
+		new_process_modal.modal('show');
+		return false;
+	});
 });
